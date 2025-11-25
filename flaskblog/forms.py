e2de2ba30238d206001955fad6390433e5a24c57
Nirmallya07 <bhowmicknirmallya07@gmail.com>
2025-11-25 21:22:20 +0530

    UpdateForm created

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm) :
 
@@ -30,3 +32,21 @@ class LoginForm(FlaskForm) :
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
     
+class UpdateForm(FlaskForm) :
+    username = StringField('Username', validators=[DataRequired(), Length(max=30, min=2)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    sublit = SubmitField("Update Info")
+
+    def validate_username(self, username) :
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user :
+                raise ValidationError("This username is already taken, please try a new username.")
+
+    def validate_email(self, email) :
+        if email.data != current_user.email :
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError("This email is already in use, plese use a different email.")
+
